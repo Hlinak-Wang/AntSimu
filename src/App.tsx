b1380c374bb4c52d31c1ef2011ab30d9165e59a9
Shili Wang <weric10032u@gmail.com>
2020-04-01 23:57:18 +1100

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import './App.css';
+
+interface IDogmsg {
+  message: string;
+  status: boolean;
+}
+function App() {
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+}
+
+export default App;
