d5dc7debd72edabccdb3a2516948e89faa4d3102
Shili Wang <weric10032u@gmail.com>
2020-04-03 18:12:55 +1100

    add button animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React, {useState} from 'react';
-import Button, {ButtonType, ButtonSize} from './components/Button/Button';
+import Button, {ButtonType, ButtonSize, ButtonHTMLType} from './components/Button/Button';
 
 function App() {
 
   return (
     <div className="App">
       <Button size={ButtonSize.large}>hello</Button>
-      <Button btnType={ButtonType.link} href="https://www.baidu.com" disabled>baidu</Button>
-      <Button btnType={ButtonType.link} href="https://www.baidu.com">baidu</Button>
+      <Button type={ButtonType.link} href="https://www.baidu.com" disabled>baidu</Button>
+      <Button type={ButtonType.link} disabled>baidu</Button>
       <Button size={ButtonSize.small}>small</Button>
-      <Button btnType={ButtonType.primary}>small</Button>
-      <Button btnType={ButtonType.primary} danger>danger</Button>
-      <Button btnType={ButtonType.primary} disabled>disabled</Button>
-      <Button disabled>disabled</Button>
+      <Button type={ButtonType.primary}>small</Button>
+      <Button type={ButtonType.primary} danger>danger</Button>
+      <Button type={ButtonType.primary} disabled HTMLType={ButtonHTMLType.button}>disabled</Button>
+      <Button onClick={() => alert("hello")} disabled>disabled</Button>
     </div>
   );
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import classnames from 'classnames';
 
 export enum ButtonSize {
@@ -12,48 +13,85 @@ export enum ButtonType {
   link = 'link',
 }
 
+export enum ButtonHTMLType {
+  button = 'button',
+  submit = 'submit',
+  reset = 'reset',
+}
+
 interface baseButtonProps {
   className ?: string;
   disabled ?: boolean;
   size?: ButtonSize;
-  btnType?: ButtonType;
-  children: React.ReactNode;
+  type?: ButtonType;
+  children?: React.ReactNode;
   href?: string;
-  danger?:boolean
+  danger?:boolean;
 }
 
-const Button: React.FC<baseButtonProps> = (props) => {
+export type NativeButtonProps = {
+    HTMLType: ButtonHTMLType;
+  }
+  & baseButtonProps
+  & Omit<React.ButtonHTMLAttributes<HTMLElement>, 'type'>;
+ 
+export type NativeAnchorProps = React.AnchorHTMLAttributes<HTMLElement> & baseButtonProps;
+
+type ButtonProps = Partial<NativeAnchorProps & NativeButtonProps>;
+
+const Button: React.FC<ButtonProps> = (props) => {
   const { 
-    btnType,
+    type,
     disabled,
     size,
     children,
     href,
-    danger
+    danger,
+    HTMLType,
+    onClick,
+    ...otherProps
   } = props;
+  
+  const [clickStatus, setStatus] = useState(true);
+
   const classes = classnames('btn', {
-    [`btn-${btnType}`]: btnType,
+    [`btn-${type}`]: type,
     [`btn-${size}`]: size,
     [`btn-danger`]: danger,
-    'disabled': (btnType === ButtonType.link) && disabled
   })
-  if (btnType === ButtonType.link) {
+
+  function handleClick() {
+    setStatus(!clickStatus)
+  };
+
+  if (href !== undefined && !disabled) {
+    const link_classes = classnames(classes, {disabled: href !== undefined && disabled})
     return (
       <a
-        className={classes}
+        className={link_classes}
         href = {href}
+        {...otherProps}
       >
         {children}
       </a>
     );
   } else {
     return (
-      <button
-        className={classes}
-        disabled = {disabled}
+      <CSSTransition
+        in={clickStatus}
+        timeout={500}
+        classNames="click"
       >
-        {children}
-      </button>
+        <button
+          className={classes}
+          disabled = {disabled}
+          type={HTMLType}
+          onClick = {handleClick}
+          {...otherProps}
+        >
+          {children}
+        </button>
+      </CSSTransition>
     )
   }
 }
@@ -61,7 +99,8 @@ const Button: React.FC<baseButtonProps> = (props) => {
 Button.defaultProps = {
   disabled: false,
   danger: false,
-  btnType: ButtonType.default
+  type: ButtonType.default,
+  HTMLType: ButtonHTMLType.button,
 }
 
 export default Button;
