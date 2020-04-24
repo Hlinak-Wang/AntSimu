196f7df9cd235a8731178fe29b98834576ca664d
Shili Wang <weric10032u@gmail.com>
2020-04-24 16:33:45 +1000

    add icon prerequire document

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
 
 
-import "../src/styles/index.scss"
+import "../src/styles/index.scss";
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+library.add(fas);
