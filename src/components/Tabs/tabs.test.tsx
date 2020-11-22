c44aefbad1ef42a222397bf3d928b2393463f987
Shili Wang <weric10032@gmail.com>
2020-11-22 15:02:37 +0800

    upload

--- src/components/Tabs/tabs.test.tsx
+++ src/components/Tabs/tabs.test.tsx
@@ -3,11 +3,14 @@ import Tabs, { TabsItem } from './tabs';
 import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react';
 
 describe('basic test', () => {
+  beforeEach(() => {
+    cleanup()
+  })
   it('should render correctly for different props TabPosition', () => {
     const wrapp = render(<Tabs type="inline" TabPosition="top">
       <TabsItem key="1" label="hello">hello</TabsItem>
       <TabsItem key="2" label="12">123</TabsItem>
     </Tabs>)
-    expect(wrapp).toMatchSnapshot();
+    expect(wrapp).toMatchSnapshot()
   })
 })
